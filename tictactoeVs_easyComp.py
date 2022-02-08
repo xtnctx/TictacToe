108ba3bd8e25e0eb7a5348e7415c7a718fbcf11c
xtnctx <ryanbahillo.10@gmail.com>
2022-02-08 21:07:04 +0800

    versus easy computer

---
+++ tictactoeVs_easyComp.py
@@ -0,0 +1,104 @@
+
+import random
+
+''' Use your NumPad to play the game '''
+
+board_sample = f'\
+|     7     |     8     |     9     | \n\
+------------------------------------- \n\
+|     4     |     5     |     6     | \n\
+------------------------------------- \n\
+|     1     |     2     |     3     |'
+
+
+# //////////////// Winning Move ////////////////
+
+win_horizontal = [
+    [6, 7, 8],
+    [3, 4, 5],
+    [0, 1, 2]
+]
+
+win_vertical = [
+    [6, 3, 0],
+    [7, 4, 1],
+    [8, 5, 2]
+]
+
+win_diagonal = [
+    [6, 4, 2],
+    [8, 4, 0]
+]
+# //////////////////////////////////////////////
+def psss():
+    print('skdskdskdsdj')
+    return 'afsdksdhfdhfskdhfskfjshfk'
+
+
+def checkWinner(player):
+    # Check Horizontal
+    for i in range(len(win_horizontal)):
+        point = 0
+        for choices in player:
+            if choices in win_horizontal[i]:
+                point += 1
+            if point == 3: return True
+
+    # Check Vertical
+    for i in range(len(win_vertical)):
+        point = 0
+        for choices in player:
+            if choices in win_vertical[i]:
+                point += 1
+            if point == 3: return True
+
+    # Check Diagonal
+    for i in range(len(win_diagonal)):
+        point = 0
+        for choices in player:
+            if choices in win_diagonal[i]:
+                point += 1
+            if point == 3: return True
+    
+    return False
+
+segment = ['']*9
+segment_player1 = []
+segment_player2 = []
+
+computer_choices = list(range(1, 10))
+
+while True:
+    player1 = int(input("Player1's turn: "))
+    computer_choices.remove(player1)
+    segment[player1-1] = 'X'
+    segment_player1.append(player1-1)
+    board = f'\
+    |     {segment[6]}     |     {segment[7]}     |     {segment[8]}     | \n\
+    ----------------------------------- \n\
+    |     {segment[3]}     |     {segment[4]}     |     {segment[5]}     | \n\
+    ----------------------------------- \n\
+    |     {segment[0]}     |     {segment[1]}     |     {segment[2]}     |'
+
+    print(board)
+
+    if checkWinner(segment_player1):
+        print('\nPlayer1 Wins!')
+        break
+
+    computer = random.choice(computer_choices)
+    computer_choices.remove(computer)
+    segment[computer-1] = 'O'
+    segment_player2.append(computer-1)
+    board = f'\
+    |     {segment[6]}     |     {segment[7]}     |     {segment[8]}     | \n\
+    ----------------------------------- \n\
+    |     {segment[3]}     |     {segment[4]}     |     {segment[5]}     | \n\
+    ----------------------------------- \n\
+    |     {segment[0]}     |     {segment[1]}     |     {segment[2]}     |'
+
+    print(board)
+
+    if checkWinner(segment_player2):
+        print('\nComputer Wins!')
+        break
